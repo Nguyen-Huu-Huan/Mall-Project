173d98fee363745ce4da98f4eb2b3b25011eba89
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-04-21 06:24:46 +0700

    Update effects.js

--- effects.js
+++ effects.js
@@ -31,3 +31,72 @@ function add_to_cart(){
   document.querySelector(".cart-qty").style.display = "inline"
   localStorage.setItem("Dunk Low Black",count)
 }
+
+var total_price = 0
+
+function price(){
+  var qty = document.querySelector(".item-qty").value
+  qty = parseInt(qty)
+  localStorage.setItem('Nike Air Max',qty)
+  var price = document.querySelector(".item-price")
+  price.innerHTML = (qty*270)+"$"
+  total_price=(qty*270)+(parseInt(localStorage.getItem("Dunk Low Black"))*130);;
+  document.querySelector(".total_money").innerHTML = total_price
+}
+
+document.querySelector(".total_money").innerHTML = 270+parseInt(localStorage.getItem("Dunk Low Black"))*130;
+function cart_load(){
+
+  var item_div_1 = document.createElement("div");
+  item_div_1.setAttribute("class","row");
+  document.querySelector(".product-space").appendChild(item_div_1)
+
+  var item_div_1_1 = document.createElement("div");
+  item_div_1_1.setAttribute("class","col-40");
+  item_div_1.appendChild(item_div_1_1)
+
+  var item_div_1_2 = document.createElement("div");
+  item_div_1_2.setAttribute("class","col-20 text-center item-price");
+  item_div_1_2.innerHTML = (parseInt(localStorage.getItem("Dunk Low Black"))*130)+"$";
+  total_price+=(parseInt(localStorage.getItem("Dunk Low Black"))*130)
+  item_div_1.appendChild(item_div_1_2)
+
+  var item_div_1_3 = document.createElement("div");
+  item_div_1_3.setAttribute("class","col-20");
+  item_div_1.appendChild(item_div_1_3)
+
+  var item_div_1_1_1 = document.createElement("div");
+  item_div_1_1_1.setAttribute("class","container");
+  item_div_1_1.appendChild(item_div_1_1_1)
+
+  var item_div_1_1_1_1 = document.createElement("div");
+  item_div_1_1_1_1.setAttribute("class","row");
+  item_div_1_1_1.appendChild(item_div_1_1_1_1)
+
+  var item_name = document.createElement("h3");
+  item_name.innerHTML = "Dunk Low Black Shoes"
+  item_name.setAttribute("class","text-center")
+  item_div_1_1_1.appendChild(item_name)
+
+  var item_img = new Image();
+  item_img.src = "images/dunklowblack6.jpg";
+  item_div_1_1_1_1.appendChild(item_img)
+
+  var item_div_1_3_1 = document.createElement("div");
+  item_div_1_3_1.setAttribute("class","row");
+  item_div_1_3.appendChild(item_div_1_3_1)
+
+  var input_number = document.createElement("input")
+  input_number.setAttribute("type","number")
+  input_number.setAttribute("class","item-qty")
+  input_number.setAttribute("value",localStorage.getItem('Dunk Low Black'))
+  input_number.addEventListener("input",function(){
+    item_div_1_2.innerHTML = (130*parseInt(input_number.value))+"$"
+    if (typeof(parseInt(input_number.value))=="number"){
+      localStorage.setItem('Dunk Low Black',parseInt(input_number.value))
+      total_price=(130*parseInt(input_number.value))+parseInt(localStorage.getItem("Nike Air Max"))*270
+      document.querySelector(".total_money").innerHTML = total_price
+    }
+  })
+  item_div_1_3_1.appendChild(input_number)
+}
