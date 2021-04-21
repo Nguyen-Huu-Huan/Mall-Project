4ca2b015d940e349ee8fb42fe6d65457033c9189
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-04-21 19:40:42 +0700

    Update effects.js

--- effects.js
+++ effects.js
@@ -128,9 +128,11 @@ function cart_load(){
 }
 function coupon_apply(){
   if (document.querySelector("input[name='coupon']").value=="COSC2430-HD"){
+    total_price = (130*parseInt(input_number.value))
     total_price *=0.8
     document.querySelector(".total-money").innerHTML = total_price+"$"
   }else if (document.querySelector("input[name='coupon']").value=="COSC2430-DI"){
+    total_price = (130*parseInt(input_number.value))
     total_price*=0.9
     document.querySelector(".total-money").innerHTML = total_price+"$"
   }else{
