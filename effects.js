9b7347a9d9ea9a3989f562cd96f903b07e188332
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-04-24 22:40:29 +0700

    Update effects.js

--- effects.js
+++ effects.js
@@ -56,7 +56,7 @@ function logged_in(){
 if (document.querySelector('.logout')!= null){
     if (localStorage.getItem('login')=='true'){
         document.querySelectorAll('.logout').forEach((button)=>{button.style.display = 'inline-block'})
-        document.querySelector("a[href='myaccount.html']").setAttribute('href','logged-in.html')
+        document.querySelectorAll("a[href='myaccount.html']:not(.logout)").setAttribute('href','logged-in.html')
         console.log(document.querySelector("a[href='myaccount.html']"))
     }else{
         document.querySelectorAll('.logout').forEach((button)=>{button.style.display = 'none'})
