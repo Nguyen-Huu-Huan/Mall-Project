d79678c3186f3f9ea510abcd9cde08c5f16f8244
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-04-25 20:32:32 +0700

    Update effects.js

--- effects.js
+++ effects.js
@@ -27,7 +27,7 @@ function myFunction() {
 
 var count = 0
 var islogin = localStorage.getItem('login')
-let cart_item = {}
+var cart_item = {}
 function add_to_cart(item) {
     if (islogin == 'true') {
         count += parseInt(document.querySelector(".shoe-qty").value)
@@ -35,14 +35,12 @@ function add_to_cart(item) {
         document.querySelectorAll(".cart-qty").forEach((p_tag) => { p_tag.innerHTML = result })
         document.querySelectorAll(".cart-qty").forEach((p_tag) => { p_tag.style.color = "red" })
         document.querySelectorAll(".cart-qty").forEach((p_tag) => { p_tag.style.display = "inline" })
-        localStorage.setItem(item, count)
-        cart_item[item] = [document.querySelector(".mobile-menu-opened>section").querySelector("div>div").children[1].children[1].textContent, count, document.querySelector(".mobile-menu-opened>section").querySelector("div>div").children[0].children[0].getAttribute('src')]
+        localStorage.setItem(item, [document.querySelector(".mobile-menu-opened>section").querySelector("div>div").children[1].children[1].textContent, count, document.querySelector(".mobile-menu-opened>section").querySelector("div>div").children[0].children[0].getAttribute('src')])
     } else {
         document.querySelector(".add-to-cart").setAttribute("href", "../myaccount.html")
         alert("Please login before purchase")
     }
 }
-console.log(cart_item)
 
 
 
