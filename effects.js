b6b136d24c77830301f36818e52f587adc396fff
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-04-25 20:46:58 +0700

    Update effects.js

--- effects.js
+++ effects.js
@@ -74,7 +74,9 @@ var total_price = 0
 
 var dunklowblack_qty = JSON.parse(localStorage.getItem("Dunk Low Black"))
 var airforceone_qty = JSON.parse(localStorage.getItem("Air Force One"))
-console.log(typeof(dunklowblack))
+for (items in localStorage){
+    console.log(items)
+}
 // Create a dictionary for iteration. The items in the array are price, name in localStorage, and image source in order
 // const cart_item = {"Dunk Low Black":[130, dunklowblack_qty, "images/dunklowblack6.jpg"],
 //                    "Air Force One":[147,airforceone_qty, "images/air-force-1-3.png"]}
