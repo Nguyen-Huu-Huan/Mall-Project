25706bb690cb286903b698c14bade419d49746b4
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-04-26 10:16:12 +0700

    Update effects.js

--- effects.js
+++ effects.js
@@ -211,6 +211,13 @@ function cart_load() {
                     airforceone_input.setAttribute("value", cart_item[items][1])
                     item_div_1_3_1.appendChild(airforceone_input)
                     airforceone_input.addEventListener("input", function(){
+                        if (airforceone_input.value==0){
+                            var remove_item= document.createElement("img");
+                            remove_item.setAttribute("src", "remove_item.png");
+                            remove_item.setAttribute("class", "col-30 remove_item");
+                            item_div_1_3_1.appendChild(remove_item)
+                        }
+                        console.log(item_div_1_3_1.children.includes("img.remove_item"))
 //                         if (item_div_1_3_1.children.includes(remove_item)){console.log('hello')}else{console.log('nothing')}
                         item_div_1_2_airforceone.innerHTML = (((JSON.parse(localStorage.getItem('Air Force One'))||0)[0]||0)*airforceone_input.value) + "$"
                         localStorage.setItem("Air Force One", JSON.stringify([147, parseInt(airforceone_input.value),((JSON.parse(localStorage.getItem('Air Force One'))||0)[2]||0)]))
