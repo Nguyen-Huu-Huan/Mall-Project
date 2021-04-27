3af2eb03a7592858adee6336b1eac020d66413da
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-04-27 07:10:08 +0700

    Update effects.js

--- effects.js
+++ effects.js
@@ -283,30 +283,30 @@ var blur_everything_not_Huy = document.querySelector('body').querySelectorAll("d
 var blur_everything_not_Huan = document.querySelector('body').querySelectorAll("div:not(div.Huan-bg-modal, div.Huan-bg-modal>*)")
 var blur_everything_not_Nguyen = document.querySelector('body').querySelectorAll("div:not(div.Nguyen-bg-modal, div.Nguyen-bg-modal>*)")
 
-document.getElementById("Huy").addEventListener("click", function(){
-    document.querySelector(".Huy-bg-modal").forEach()style.display = "flex";
-    blur_everything_not_Huy.forEach((div)=>{div.style.opacity = "0.651"})
-});
-document.getElementById("Huymodal").addEventListener("click", function(){
-    document.querySelector(".Huy-bg-modal").style.display = "none";
-//     blur_everything_not_Huy.forEach((div)=>{div.style.opacity = "1"})
-});
-//modal window's Huan
-document.getElementById("Huan").addEventListener("click", function(){
-    document.querySelector(".Huan-bg-modal").style.display = "flex";
-    blur_everything_not_Huan.forEach((div)=>{div.style.opacity = "0.651"})
-});
-document.getElementById("Huanmodal").addEventListener("click", function(){
-    document.querySelector(".Huan-bg-modal").style.display = "none";
-//     blur_everything_not_Huan.forEach((div)=>{div.style.opacity = "1"})
-});
-//modal window's Nguyen
-document.getElementById("Nguyen").addEventListener("click", function(){
-    document.querySelector(".Nguyen-bg-modal").style.display = "flex";
-    blur_everything_not_Nguyen.forEach((div)=>{div.style.opacity = "0.651"})
-});
-document.getElementById("Nguyenmodal").addEventListener("click", function(){
-    document.querySelector(".Nguyen-bg-modal").style.display = "none";
-//     blur_everything_not_Nguyen.forEach((div)=>{div.style.opacity = "1"})
-});
+// document.getElementById("Huy").addEventListener("click", function(){
+//     document.querySelector(".Huy-bg-modal").forEach()style.display = "flex";
+//     blur_everything_not_Huy.forEach((div)=>{div.style.opacity = "0.651"})
+// });
+// document.getElementById("Huymodal").addEventListener("click", function(){
+//     document.querySelector(".Huy-bg-modal").style.display = "none";
+// //     blur_everything_not_Huy.forEach((div)=>{div.style.opacity = "1"})
+// });
+// //modal window's Huan
+// document.getElementById("Huan").addEventListener("click", function(){
+//     document.querySelector(".Huan-bg-modal").style.display = "flex";
+//     blur_everything_not_Huan.forEach((div)=>{div.style.opacity = "0.651"})
+// });
+// document.getElementById("Huanmodal").addEventListener("click", function(){
+//     document.querySelector(".Huan-bg-modal").style.display = "none";
+// //     blur_everything_not_Huan.forEach((div)=>{div.style.opacity = "1"})
+// });
+// //modal window's Nguyen
+// document.getElementById("Nguyen").addEventListener("click", function(){
+//     document.querySelector(".Nguyen-bg-modal").style.display = "flex";
+//     blur_everything_not_Nguyen.forEach((div)=>{div.style.opacity = "0.651"})
+// });
+// document.getElementById("Nguyenmodal").addEventListener("click", function(){
+//     document.querySelector(".Nguyen-bg-modal").style.display = "none";
+// //     blur_everything_not_Nguyen.forEach((div)=>{div.style.opacity = "1"})
+// });
 
