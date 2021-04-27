deb5682c67c9d657fb34a56a982e781a9a17307b
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-04-28 00:53:55 +0700

    Update effects.js

--- effects.js
+++ effects.js
@@ -37,6 +37,7 @@ function add_to_cart(item) {
         document.querySelectorAll(".cart-qty").forEach((p_tag) => { p_tag.style.display = "inline" })
         localStorage.setItem(item, JSON.stringify([parseInt(document.querySelector(".mobile-menu-opened>section").querySelector("div>div").children[1].children[1].textContent.substring(1)), count, document.querySelector(".mobile-menu-opened>section").querySelector("div>div").children[0].children[0].getAttribute('src')]))
         document.querySelector(".cart-icon").style.animation = "shaking 0.5s infinite"
+        document.querySelector(".cart-icon-big").style.animation = "shaking 0.5s infinite"
     } else {
         document.querySelector(".add-to-cart").setAttribute("href", "../myaccount.html")
         alert("Please login before purchase")
