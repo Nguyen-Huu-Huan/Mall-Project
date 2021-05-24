f544c1d27061b508a664d024b6637f4139f592c9
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-05-25 01:06:41 +0700

    Update logged-in.php

--- logged-in.php
+++ logged-in.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<?php session_start()?>
+<?php $_SESSION['logged-in'] = true;?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
