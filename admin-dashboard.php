d172b6a68bc7030bfb83a8e913bff8e7b6ec2e4e
Your Name <you@example.com>
2021-05-25 19:40:59 +0700

    update

---
+++ admin-dashboard.php
@@ -0,0 +1,40 @@
+<?php
+$texts = file('data.csv');
+str_replace('&#xFEFF', " asd", $texts[0]);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mallux About us | Online Shopping Mall</title>
+  <link rel="stylesheet" href="style.css">
+
+  <link rel="preload" href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;500;800&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'" />
+  <noscript>
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;500;800&display=swap" rel="stylesheet" type="text/css" />
+  </noscript>
+</head>
+
+<body>
+  <section class="admin-dashboard">
+    <h1 class="section-title">Admin Dashboard</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-100">
+        <label for="copyright-text">Copyright Texts</label>
+        <textarea name="copyright-text" placeholder=""><?php echo str_replace('"', '', $texts[0]); ?></textarea>
+        </div>
+        <div class="col-30">
+        </div>
+        <div class="col-30">
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$file = fopen("data.csv","r");
-  $result = array_column(array_map('str_getcsv', file('data.csv')), 1, 0);
-fclose($file);
-echo nl2br ($result['ToS']);
-?>
