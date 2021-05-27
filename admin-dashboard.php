cca3f7c44470ca557e2be996cea9ccbffc898185
Your Name <you@example.com>
2021-05-27 11:54:31 +0700

    update

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -1,14 +1,15 @@
-<?php 
-# Don't delete, PHP01
-if (file_exists('install.php') === TRUE) {die('Error, the file install.php is still exists');}
-?>
 <?php
 $fp = fopen('data.csv', 'r');
 while ($line = fgets($fp)) {
   $texts[] = $line;
 };
 
-print_r($texts);
+$fp = fopen('../adminfile.txt', 'r');
+while ($line = fgets($fp)) {
+  $admin[] = $line;
+};
+
+
 
 if (isset($_POST['admin-dashboard'])) {
   echo "<meta http-equiv='refresh' content='0'>";
@@ -24,10 +25,13 @@ if (isset($_POST['admin-dashboard'])) {
 
 ?>
 
-<?php 
+<?php
 # Don't delete, PHP01
-if (file_exists('install.php') === TRUE) {die('Error, the file install.php is still exists');}
+if (file_exists('install.php') === TRUE) {
+  die('Error, the file install.php is still exists');
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -43,32 +47,90 @@ if (file_exists('install.php') === TRUE) {die('Error, the file install.php is st
     <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;500;800&display=swap" rel="stylesheet" type="text/css" />
   </noscript>
 </head>
+<?php
+$yoa = 'zxc';
+session_start();
+echo ($_SESSION['valid']);
+echo ($yoa = $admin[0] && password_verify('zxc', $admin[1]));
 
+?>
+
+<section class="admin-reg">
+  <h1 class="section-title">Admin login</h1>
+  <div class="small-container">
+    <div class="row">
+      <div class="col-50">
+        <form method="post" name="admin-login-form" onsubmit="return passCheck()">
+          <label for="admin-username">Username</label><br>
+          <input style="width: 100%;" type="text" name="admin-username" required><br><br>
+          <label for="admin-password">Password</label><br>
+          <input style="width: 100%;" type="password" name="admin-password" id="password" required><br><br>
+          <?php
+
+          if (isset($_POST['admin-login'])) {
+            if (
+              $_POST['admin-username'] != $admin[0] || password_verify($_POST['admin-password'], $admin[1]) === false
+            ) {
+              $_SESSION['valid'] === false;
+              echo "<meta http-equiv='refresh' content='0'>";
+            }
+            if (
+              $_POST['admin-username'] = $admin[0] && password_verify($_POST['admin-password'], $admin[1])
+            ) {
+              $_SESSION['valid'] === true;
+              echo "<meta http-equiv='refresh' content='0'>";
+            }
+          }
+          ?>
+          <br><br>
+          <button id="submit" class="btn" style="width: 30%;" type="submit" name="admin-login">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php
+
+if ($_SESSION['valid'] === true) {
+  echo "
 <body>
-  <section class="admin-dashboard">
-    <h1 class="section-title">Admin Dashboard</h1>
-    <p class="color-red text-center">Use &ltbr&gt for line break and remember to click save after editing.</p>
-    <div class="container">
-      <div class="row">
-        <div class="col-100">
-          <form method="post" name="admin-dashboard-form" action="">
-            <div style="margin:0 35%">
-              <button class="btn" style="width: 100%;" type="submit" name="admin-dashboard">Save</button><br>
+  <section class='admin-dashboard'>
+    <h1 class='section-title'>Admin Dashboard</h1>
+    <p class='color-red text-center'>Use &ltbr&gt for line break and remember to click save after editing.</p>
+    <br>
+    <div class='container'>
+      <div class='row'>
+        <div class='col-100'>
+          <form method='post' name='admin-dashboard-form' action=''>
+            <label class='text-big' for='copyright-text'>Copyright Texts</label>
+            <textarea class='ad-textarea' name='copyright-text' placeholder=''>";
+  echo str_replace("<br>", "", $texts[0]);
+  echo "</textarea><br>
+
+            <label class='text-big' for='tos-text'>Term Of Services</label>
+            <textarea class='ad-textarea' name='tos-text' placeholder=''>";
+  echo str_replace("<br>", "", $texts[1]);
+  echo "</textarea><br>
+
+            <label class='text-big' for='cprivacy-text'>Privacy Policy</label>
+            <textarea class='ad-textarea' name='privacy-text' placeholder=''>";
+  echo str_replace("<br>", "", $texts[2]);
+  echo "</textarea><br>
+            <div style='margin:0 35%'>
+            <form method='post'>
+            <button style='width: 45%;' class='btn bg-color-black' type='submit' name='logout'>Logout</button>
+</form>
+            
+              <button class='btn' style='width: 45%;' type='submit' name='admin-dashboard'>Save</button><br>
 
             </div>
-            <label class="text-big" for="copyright-text">Copyright Texts</label>
-            <textarea class="ad-textarea" name="copyright-text" placeholder=""><?php echo str_replace("<br>", "", $texts[0]) ?></textarea><br>
-
-            <label class="text-big" for="tos-text">Term Of Services</label>
-            <textarea class="ad-textarea" name="tos-text" placeholder=""><?php echo str_replace("<br>", "", $texts[1]) ?></textarea><br>
-
-            <label class="text-big" for="cprivacy-text">Privacy Policy</label>
-            <textarea class="ad-textarea" name="privacy-text" placeholder=""><?php echo str_replace("<br>", "", $texts[2]) ?></textarea><br>
           </form>
         </div>
       </div>
     </div>
   </section>
-</body>
+</body>";
+} ?>
 
 </html>
---
+++ instaall.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mallux About us | Online Shopping Mall</title>
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;500;800&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'" />
+    <noscript>
+        <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;500;800&display=swap" rel="stylesheet" type="text/css" />
+    </noscript>
+</head>
+
+<body>
+    <section class="admin-reg">
+        <h1 class="section-title">Admin Registeration</h1>
+        <div class="small-container">
+            <div class="row">
+                <div class="col-50">
+                    <form method="post" name="admin-reg-form" onsubmit="return passCheck()">
+                        <label for="admin-username">Username</label><br>
+                        <input style="width: 100%;" type="text" name="admin-username" required><br><br>
+                        <label for="admin-password">Password</label><br>
+                        <input style="width: 100%;" type="password" name="admin-password" onkeyup="passCheck()" id="password" required><br><br>
+                        <label for="re-password">Repeat Password</label><br>
+                        <input style="width: 100%;" type="password" name="re-password" onkeyup="passCheck()" id="re-password" required>
+                        <p id="error"></p>
+                        <br><br>
+                        <button id="submit" class="btn" style="width: 30%;" type="submit" name="admin-reg">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+<script>
+
+    let message = ""
+
+    function passCheck() {
+        if (document.querySelector("#password").value == document.querySelector("#re-password").value) {
+
+            if (document.querySelector("#password").value == "") {
+                document.querySelector("#error").innerHTML = "";
+            } else {
+                message = "Password matched"
+                document.querySelector("#error").innerHTML = message;
+                return true;
+            }
+
+            <?php
+            if (isset($_POST['admin-reg'])) {
+                $username = $_POST['admin-username'];
+                $password = $_POST['admin-password'];
+                $password_hash = password_hash($password, PASSWORD_BCRYPT);
+                $adminfile = fopen('../adminfile.txt', 'w');
+                fwrite($adminfile, $username . "\n");
+                fwrite($adminfile, $password_hash);
+            }
+            ?>
+
+        } else {
+            if (document.querySelector("#password").value == "") {
+                document.querySelector("#error").innerHTML = "";
+            } else {
+                message = "Password does not match"
+                document.querySelector("#error").innerHTML = message;
+            }
+            return false;
+        }
+    }
+
+</script>
+
+</html>
