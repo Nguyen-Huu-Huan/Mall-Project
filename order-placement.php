ccd21b4c476400f0402b80ac01ac943122131bab
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-05-29 02:49:41 +0700

    Update order-placement.php

--- order-placement.php
+++ order-placement.php
@@ -5,6 +5,7 @@ if (file_exists('install.php') === TRUE) {die('Error, the file install.php is st
 <!DOCTYPE html>
 <html lang="en">
 <?php 
+    session_start();
     echo isset($_SESSION['logged-in']);
     echo $_SESSION['logged-in'];
     if (isset($_POST['order'])){
