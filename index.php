8fdbc326af29759f09d68499c3ae93cea04c12de
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-05-29 03:50:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -208,7 +208,6 @@ if (file_exists('install.php') === TRUE) {die('Error, the file install.php is st
                 }
                 echo "</div>
                 </div>";
-                print_r($created_time);
                 foreach($created_time as $store){
                     echo $store[1].nl2br("\n");
                 }
