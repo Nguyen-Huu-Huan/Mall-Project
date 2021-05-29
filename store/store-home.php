160ff77ac729a9bfcde5bd574e9910297f305f5e
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-05-29 16:58:42 +0700

    Update store-home.php

--- store/store-home.php
+++ store/store-home.php
@@ -178,34 +178,34 @@ if (file_exists('install.php') === TRUE) {die('Error, the file install.php is st
 //                 }
 //                 echo "</div>";
 //                 echo "</div>";
-                echo "<a href='#' id='display' onclick='see_products()'>Click here to see all products</a>";
-                echo "<a href='#' id='collapse' onclick='product_disappear()' style=\"display:none\">Collapse table</a>";
-                echo "<script type='text/javascript'>function see_products(){
-                    document.querySelector('.see_all').style.display='block';
-                    document.querySelector('#collapse').style.display='block';
-                    document.querySelector('#display').style.display='none';
-                    document.querySelector('#collapse').addEventListener(\"click\", function(event){
-                        event.preventDefault()});
-                }</script>";
-                echo "<script type='text/javascript'>function product_disappear(){
-                    document.querySelector('.see_all').style.display='none';
-                    document.querySelector('#collapse').style.display='none';
-                    document.querySelector('#display').style. display='block';
-                    document.querySelector('#display').addEventListener(\"click\", function(event){
-                        event.preventDefault()});
-                }</script>";
-                echo "<table cellspacing='20' class='small-container see_all' style=\"display:none\">";
-                for ($i = 0; $i<count($feature_products);$i+=5){
-                    echo "<tr style='text-align: center;'>";
-                    for ($j = 0; $j < 5; $j += 1){
-                        if ($i+$j<count($feature_products)){
-                            $cell = $feature_products[$i+$j][1];
-                            echo "<td>$cell</td>";
-                        }
-                    }
-                    echo "</tr>";
-                }
-                echo "</table>";
+//                 echo "<a href='#' id='display' onclick='see_products()'>Click here to see all products</a>";
+//                 echo "<a href='#' id='collapse' onclick='product_disappear()' style=\"display:none\">Collapse table</a>";
+//                 echo "<script type='text/javascript'>function see_products(){
+//                     document.querySelector('.see_all').style.display='block';
+//                     document.querySelector('#collapse').style.display='block';
+//                     document.querySelector('#display').style.display='none';
+//                     document.querySelector('#collapse').addEventListener(\"click\", function(event){
+//                         event.preventDefault()});
+//                 }</script>";
+//                 echo "<script type='text/javascript'>function product_disappear(){
+//                     document.querySelector('.see_all').style.display='none';
+//                     document.querySelector('#collapse').style.display='none';
+//                     document.querySelector('#display').style. display='block';
+//                     document.querySelector('#display').addEventListener(\"click\", function(event){
+//                         event.preventDefault()});
+//                 }</script>";
+//                 echo "<table cellspacing='20' class='small-container see_all' style=\"display:none\">";
+//                 for ($i = 0; $i<count($feature_products);$i+=5){
+//                     echo "<tr style='text-align: center;'>";
+//                     for ($j = 0; $j < 5; $j += 1){
+//                         if ($i+$j<count($feature_products)){
+//                             $cell = $feature_products[$i+$j][1];
+//                             echo "<td>$cell</td>";
+//                         }
+//                     }
+//                     echo "</tr>";
+//                 }
+//                 echo "</table>";
 
             ?>
         
