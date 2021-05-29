addedf25609c30b0087077375952193fcd97be7a
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-05-30 01:50:08 +0700

    Update browse-by-name.php

--- browse-by-name.php
+++ browse-by-name.php
@@ -181,7 +181,7 @@ if (file_exists('install.php') === TRUE) {die('Error, the file install.php is st
 //                         echo $line[1].nl2br("\n");
 //                         echo (strpos($line[1], strtoupper($letter))).nl2br("\n");
 //                         echo (strpos($line[1], "F")==0).nl2br("\n")
-                        echo strpos($line[1],"F");
+                        echo strpos($line[1],"F").nl2br("\n");
                     }
                 }
 //                 echo $line[1].nl2br("\n");
