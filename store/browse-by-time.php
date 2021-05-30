7c287438a32514df6fe7f4163912e44ebeec6f3b
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-05-31 06:34:52 +0700

    Update browse-by-time.php

--- store/browse-by-time.php
+++ store/browse-by-time.php
@@ -221,7 +221,6 @@ if (file_exists('install.php') === TRUE) {die('Error, the file install.php is st
                             echo "<form action='' method='POST'><input type='submit' name='next' value='See more'></form>";
                         }
                         echo "</div>";
-
                     }
                 }     
                 echo "</div>";
@@ -239,22 +238,8 @@ if (file_exists('install.php') === TRUE) {die('Error, the file install.php is st
                     document.querySelector('#display').addEventListener(\"click\", function(event){
                         event.preventDefault()});
                 }</script>";
-                echo "<script type='text/javascript'>function see_products(){
-                    document.querySelector('.see_all').style.display='block';
-                    document.querySelector('#collapse').style.display='block';
-                    document.querySelector('#display').style.display='none';
-                    document.querySelector('#collapse').addEventListener(\"click\", function(event){
-                        event.preventDefault()});
-                }</script>";
-                echo "<script type='text/javascript'>function product_disappear(){
-                    document.querySelector('.see_all').style.display='none';
-                    document.querySelector('#collapse').style.display='none';
-                    document.querySelector('#display').style. display='block';
-                    document.querySelector('#display').addEventListener(\"click\", function(event){
-                        event.preventDefault()});
-                }</script>";
-                echo "<a href='' id='display' onclick='see_products()'>Click here to see all products</a>";
-                echo "<a href='' id='collapse' onclick='product_disappear()' style=\"display:none\">Collapse table</a>";
+                echo "<a id='display' onclick='see_products()'>Click here to see all products</a>";
+                echo "<a id='collapse' onclick='product_disappear()' style=\"display:none\">Collapse table</a>";
                 echo "<div class='tiny-container see_all' style=\"display:none;\">";
                 echo "<table rules='all' class='text-center'>";
                 echo "<tr>";
