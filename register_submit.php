4fd62263cc8c5dee2d5c64f56c35171ba754d659
s3879343 <s3879343@rmit.edu.vn>
2021-05-30 16:52:54 +0700

    Update register_submit.php

--- register_submit.php
+++ register_submit.php
@@ -1,16 +1,26 @@
 <?php
+include 'config.php';
     if( isset($_POST['submit']) && $_POST["email"] != ''&& $_POST["phone"] != ''&& $_POST["pass"] != ''&& $_POST["cfpass"] != ''&& $_POST["fname"] != ''&& $_POST["lname"] != ''&& $_POST["address"] != ''&& $_POST["city"] != '' ){
         $email = $_POST["email"];
         $phone = $_POST["phone"];
+        $fname = $_POST["fname"];
+        $lname = $_POST["lname"];
+        $address = $_POST["address"];
+        $city = $_POST["city"];
         $pass = $_POST["pass"];
         $cfpass = $_POST["cfpass"];
         if( $pass != $cfpass){
             header("location:register.php");
         }
-        $fname = $_POST["fname"];
-        $lname = $_POST["lname"];
-        $address = $_POST["address"];
-        $city = $_POST["city"];
+        $sql = "SELECT * FROM users WHERE email = '$email' ";
+        $old = mysqli_query($conn,$sql);
+        $pass= md5($pass);
+        if (mysqli_num_rows($old) > 0){
+            header("location:register.php");
+        }
+        $sql = "INSERT INTO users (email,pass,phone,fname,lname,address,city) VALUES ('$email','$pass','$phone','$fname','$lname','$address','$city')";
+        mysqli_query($conn,$sql);
+        header("location:myacount.php");
     }else{
         header("location:register.php");
     }
