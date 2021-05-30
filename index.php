215c00c40c00de7e9cc9c5162c74edb2b167dabc
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-05-31 06:28:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -274,8 +274,8 @@ if (file_exists('install.php') === TRUE) {die('Error, the file install.php is st
                 }
                 echo "</div>
                 </div>";
-                echo "<a href='#' id='display_fproduct' onclick='see_fproduct()'>Click here to see the 10 latest featured products</a>";
-                echo "<a href='#' id='collapse_fproduct' onclick='fproduct_disappear()' style=\"display:none\">Collapse table</a>";
+                echo "<a id='display_fproduct' onclick='see_fproduct()'>Click here to see the 10 latest featured products</a>";
+                echo "<a id='collapse_fproduct' onclick='fproduct_disappear()' style=\"display:none\">Collapse table</a>";
                 echo "<script type='text/javascript'>function see_fproduct(){
                     document.querySelector('.see_all_fproduct').style.display='block';
                     document.querySelector('#collapse_fproduct').style.display='block';
@@ -345,8 +345,8 @@ if (file_exists('install.php') === TRUE) {die('Error, the file install.php is st
                 }
                 echo "</div>
                 </div>";
-                echo "<a href='#' id='display_nstore' onclick='see_nstore()'>Click here to see the 10 latest new stores</a>";
-                echo "<a href='#' id='collapse_nstore' onclick='nstore_disappear()' style=\"display:none\">Collapse table</a>";
+                echo "<a id='display_nstore' onclick='see_nstore()'>Click here to see the 10 latest new stores</a>";
+                echo "<a id='collapse_nstore' onclick='nstore_disappear()' style=\"display:none\">Collapse table</a>";
                 echo "<script type='text/javascript'>function see_nstore(){
                     document.querySelector('.see_all_nstore').style.display='block';
                     document.querySelector('#collapse_nstore').style.display='block';
